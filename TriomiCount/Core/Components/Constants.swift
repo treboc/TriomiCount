6b170148657584612c6d18b300519d917b35021e
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-06-19 11:30:28 +0200

    Change height and radius to a multiple of 8

--- TriomiCount/Core/Components/Constants.swift
+++ TriomiCount/Core/Components/Constants.swift
@@ -8,6 +8,6 @@
 import SwiftUI
 
 enum Constants {
-  static let cornerRadius: CGFloat = 15
-  static let buttonHeight: CGFloat = 45
+  static let cornerRadius: CGFloat = 16
+  static let buttonHeight: CGFloat = 48
 }
