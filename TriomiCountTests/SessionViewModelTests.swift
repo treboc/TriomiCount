5a01fc4403fe90d5673e041cbe3b1f2989a4c0c3
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-07-09 00:02:45 +0200

    Rebuild tests

--- TriomiCountTests/SessionViewModelTests.swift
+++ TriomiCountTests/SessionViewModelTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import TriomiCount
+import SwiftUI
 
 class SessionOnboardingViewModelTests: XCTestCase {
   var coreDataManager: CoreDataManager!
@@ -22,53 +23,64 @@ class SessionOnboardingViewModelTests: XCTestCase {
     sut = nil
   }
 
-//  func test_toggleChosenState_shouldAddPlayerToChosenPlayers() {
-//    coreDataManager.addPlayer("Dummy", context: coreDataManager.context)
-//    let player = coreDataManager.getAllPlayers(coreDataManager.context).first!
-//
-//    player.toggleIsChosenStatus()
-//
-//    let expected = Player.getAllChosenPlayers(coreDataManager.context).contains(player)
-//
-//    XCTAssertTrue(expected)
-//  }
-//
-//  func test_toggleChosenState_ifPlayerIsChosen_PositionShouldNotBe0() {
-//    coreDataManager.addPlayer("Dummy", context: coreDataManager.context)
-//    let player = coreDataManager.getAllPlayers(coreDataManager.context).first!
-//
-//    sut.toggleIsChosenState(player)
-//
-//    let expected: Int16 = 1
-//    XCTAssertEqual(expected, player.position)
-//  }
-//
-//  func test_chosenPlayers_positionShouldMatchArray() {
-//    // Create 5 players to test on
-//    for index in 0..<5 {
-//      coreDataManager.addPlayer("Dummy \(index)", context: coreDataManager.context)
-//    }
-//    let players = coreDataManager.getAllPlayers(coreDataManager.context)
-//
-//    // Add them all to the chosenPlayersArray and give them a position
-//    for player in players {
-//      sut.toggleIsChosenState(player)
-//    }
-//
-//    let result = players.compactMap { $0.position }
-//    let expected: [Int16] = [1, 2, 3, 4, 5]
-//
-//    XCTAssertEqual(result, expected)
-//  }
-//
-//  func test_toggleChosenState_ifPlayerIsNotChosen_PositionShouldBe0() {
-//    coreDataManager.addPlayer("Dummy", context: coreDataManager.context)
-//    let player = coreDataManager.getAllPlayers(coreDataManager.context).first!
-//
-//    sut.toggleIsChosenState(player)
-//    sut.toggleIsChosenState(player)
-//
-//    let expected: Int16 = 0
-//    XCTAssertEqual(expected, player.position)
-//  }
+  func test_toggleChosenState_shouldAddPlayerToChosenPlayers() {
+    PlayerService.addNewPlayer("Dummy", in: coreDataManager.context)
+    let player = PlayerService.allObjects(Player.self, in: coreDataManager.context).first!
+
+    sut.choose(player)
+
+    let expected = sut.chosenPlayers.contains(player)
+
+    XCTAssertTrue(expected)
+  }
+
+  func test_toggleChosenState_ifOnePlayerIsChosen_PositionShouldBeOne() {
+    PlayerService.addNewPlayer("Dummy", in: coreDataManager.context)
+    let player = PlayerService.allObjects(Player.self, in: coreDataManager.context).first!
+
+    sut.choose(player)
+
+    let expected: Int16 = 1
+    XCTAssertEqual(expected, player.position)
+  }
+
+  func test_choose_if5PlayersAreChosen_positionShouldMatch() {
+    // Create 5 players to test on
+    for index in 0..<5 {
+      PlayerService.addNewPlayer("Dummy \(index)", in: coreDataManager.context)
+    }
+
+    let players = PlayerService.allObjects(Player.self, in: coreDataManager.context)
+
+    // Add them all to the chosenPlayersArray and give them a position
+    for player in players {
+      sut.choose(player)
+    }
+
+    let result = players.compactMap { $0.position }
+    let expected: [Int16] = [1, 2, 3, 4, 5]
+
+    XCTAssertEqual(result, expected)
+  }
+
+  func test_choose_callTwice_shouldResetPlayersPositionTo0() {
+    PlayerService.addNewPlayer("Dummy", in: coreDataManager.context)
+    let player = PlayerService.allObjects(Player.self, in: coreDataManager.context).first!
+
+    sut.choose(player)
+    sut.choose(player)
+
+    let expected: Int16 = 0
+    XCTAssertEqual(expected, player.position)
+  }
+
+  func test_choose_callTwice_shouldRemovePlayerFromChosenPlayers() {
+    PlayerService.addNewPlayer("Dummy", in: coreDataManager.context)
+    let player = PlayerService.allObjects(Player.self, in: coreDataManager.context).first!
+
+    sut.choose(player)
+    sut.choose(player)
+
+    XCTAssertFalse(sut.chosenPlayers.contains(player))
+  }
 }
