58613ec93da0742e949fca920b10537d7be3b5ab
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-07-09 00:01:47 +0200

    Make backgroundGradient slightly less opaque

--- TriomiCount/Core/Components/Background.swift
+++ TriomiCount/Core/Components/Background.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Background: View {
   var body: some View {
-    LinearGradient(colors: [.primaryBackground.opacity(0.8), .primaryBackground.opacity(0.2)],
+    LinearGradient(colors: [.primaryBackground.opacity(0.6), .primaryBackground.opacity(0.1)],
                    startPoint: .top,
                    endPoint: .bottom)
     .ignoresSafeArea()
