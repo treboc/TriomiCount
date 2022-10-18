6142c1b6b53909bc7efbf9f760e56bdbcd235ae3
Marvin Lee Kobert <marvin.kobert@me.com>
2022-10-18 19:12:45 +0200

    [Ref.] rebuild modifier, remove introspect

--- TriomiCount/Core/Components/UI Components/ViewModifier/RoundedNavigationTitle-ViewModifier.swift
+++ TriomiCount/Core/Components/UI Components/ViewModifier/RoundedNavigationTitle-ViewModifier.swift
@@ -17,17 +17,18 @@ extension View {
 struct RoundedNavigationTitle: ViewModifier {
   func body(content: Content) -> some View {
     content
-      .introspectNavigationController { navController in
-        var titleFont = UIFont.preferredFont(forTextStyle: .largeTitle)
-        titleFont = UIFont(descriptor:
-                            titleFont.fontDescriptor
-          .withDesign(.rounded)?
-          .withSymbolicTraits(.traitBold)
-                           ??
-                           titleFont.fontDescriptor,
-                           size: titleFont.pointSize
-        )
-        navController.navigationBar.largeTitleTextAttributes = [.font: titleFont]
-      }
+  }
+
+  init() {
+    var titleFont = UIFont.preferredFont(forTextStyle: .largeTitle)
+    titleFont = UIFont(descriptor:
+                        titleFont.fontDescriptor
+      .withDesign(.rounded)?
+      .withSymbolicTraits(.traitBold)
+                       ??
+                       titleFont.fontDescriptor,
+                       size: titleFont.pointSize
+    )
+    UINavigationBar.appearance().largeTitleTextAttributes = [.font: titleFont]
   }
 }
